f6d107232c388b43fa8b0cedbc83ceb16d4f100e
SalinaK1 <saleena.koirala1@gmail.com>
2020-11-03 13:27:55 +0545

    Basic Network scanner project initialized.

---
+++ network_scanner.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+import argparse
+# from scapy.layers.net import IP, ICMP
+
+def get_argument():
+    parser= argparse.ArgumentParser()
+    parser.add_argument('-t','--target',dest ='target', help='Target IP address/range.')
+    argument = parser.parse_args()
+    print("1")
+    print(argument.target)
+
+    if not argument.target:   #Check for errors i.e if the user does not specify the target IP Address
+        parser.error("[-] Please specify target IP address/ range. Use --help for more information.")       #Display the error mesage and quit the program if the argument is missing.
+        print("2")
+    return argument
+
+target = get_argument()
+print("3")
+print (target)
